d86a35aae60095838774bf6f29abe9aaac22dd40
DiegoMauroy <diego.mauroy@gmail.com>
2023-02-04 11:46:51 +0100

    add dictionnary_to_dataframe and select_in_boolean_column

--- src/main.py
+++ src/main.py
@@ -24,9 +24,20 @@ if __name__ == "__main__":
     if user_identified == True: # not a necessary condition, but just to be sure
 
         # call api and recover data "currencies"
-        status_currencies, data_currency_code = make_request(url_base, url_query)
+        status_currencies_code, data_currency_code = make_request(url_base, url_query)
 
         # check connection api
-        if status_currencies != 200:
+        if status_currencies_code != 200:
 
             print("Error API")
+        
+        else:
+
+            # organize data into a dataframe
+            df_currency_code = dictionnary_to_dataframe(data_currency_code["data"]["currencies"])
+
+            # Reduce the dataset to rows where "altcoing" == False
+            df_currency_code = select_in_boolean_column(df_currency_code, "altcoin", False)
+
+            # save dataset
+            df_currency_code.to_csv("data/df_currency_code.csv")
