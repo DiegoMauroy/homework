8d84d45328724a95426daf5b217f411613f6cb84
DiegoMauroy <diego.mauroy@gmail.com>
2023-02-04 11:47:38 +0100

    add dictionnary_to_dataframe and select_in_boolean_column

--- src/my_library.py
+++ src/my_library.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 #### The function returns the data generated from an API call to url_base + query_url + status of request ####
 #### Data are in a dictonnary ####
@@ -29,3 +30,23 @@ def verification_user(user_name, password):
 
         print("Mot de passe et/ou nom d'utilisateur incorrect(s)")
         return False
+    
+#### Transforms a dictionnary into a dataframe ####
+def dictionnary_to_dataframe(data, transpose=True):
+
+    df = pd.DataFrame(data)
+
+    if transpose:
+        
+        df = df.T
+
+    return df
+
+#### Selects the rows of the dataframe where col == bool ####
+#### col is the name of a boolean column ####
+#### bool is a boolean ####
+def select_in_boolean_column(df, col, bool):
+
+    df_reduce = df[df[col] == bool]
+
+    return df_reduce
