70b001af93f7737c91864844d767d7abf876a356
DiegoMauroy <diego.mauroy@gmail.com>
2023-02-05 11:33:49 +0100

    generalization of the resample function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import configparser
 #### Main ####
 if __name__ == "__main__":
 
-    # Read config file
+    # read config file
     config = configparser.ConfigParser()
     config.read('config.ini')
     user_name = config['DEFAULT']['user_name']
@@ -23,4 +23,23 @@ if __name__ == "__main__":
     
     if user_identified == True: # not a necessary condition, but just to be sure
 
-        resample(creation_df_final, url_base, url_query, url, w=0, h=0, m=10)
+        params_url1 = {"url_base" : url_base, 
+                       "url_query" : url_query, 
+                       "filename" : "data/df_currency_code", 
+                       "list_index_json": ["data", "currencies"], 
+                       "timestamp_bool" : False, 
+                       "boolean_column_name" : "altcoin", 
+                       "boolean_column_bool" : False, 
+                       "column_dict_name" : None, 
+                       "column_dict_replace" : True}
+        params_url2 = {"url_base" : url_base, 
+                       "url_query" : url, 
+                       "filename" : "data/df_price", 
+                       "list_index_json": [], 
+                       "timestamp_bool" : True, 
+                       "boolean_column_name" : None, 
+                       "boolean_column_bool" : False, 
+                       "column_dict_name" : "rates", 
+                       "column_dict_replace" : True}
+        
+        resample(creation_df_final, params_url1, params_url2, "data/df_final", w=0, h=0, m=10)
--- src/my_library.py
+++ src/my_library.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import schedule
 
-#### The function returns the data generated from an API call to url_base + query_url + status of request ####
+#### The function returns the data generated from an API call (url_base + query_url) and status of request ####
 #### Data are in a dictonnary ####
 def make_request(url_base, query_url):
 
@@ -95,58 +95,68 @@ def resample(fct, *args, w=0, h=0, m=0):
 
         schedule.run_pending()
 
-#### Principal function to creat df_final ####
-def creation_df_final(url_base, url_query1, url_query2):
+#### Get data api, put data in dataframe, organize dataframe and save dataframe ####
+def data_api_to_dataframe(url_base, url_query, filename, list_index_json = [], timestamp_bool = False, boolean_column_name = None, boolean_column_bool = False, column_dict_name = None, column_dict_replace = True):
 
-    # call api and recover data "currencies"
-    status_currencies_code, data_currency_code = make_request(url_base, url_query1)
+    # call the api and put the data in the dictionary
+    if timestamp_bool == True:
+        
+        time_api_call = timestamp()
+
+    status, data = make_request(url_base, url_query)
 
     # check connection api
-    if status_currencies_code != 200:
+    if status != 200:
 
         print("Error API")
+
+        return None, status
     
     else:
 
         # organize data into a dataframe
-        df_currency_code = dictionnary_to_dataframe(data_currency_code["data"]["currencies"])
+        for index_json in list_index_json:
+            
+            data = data[index_json]
 
-        # Reduce the dataset to rows where "altcoing" == False
-        df_currency_code = select_in_boolean_column(df_currency_code, "altcoin", False)
+        df = dictionnary_to_dataframe(data)
 
-        # save dataset
-        df_currency_code.to_parquet("data/df_currency_code.gzip", compression='gzip')
-    
-    # call api and recover data "ticker-all-currencies"
-    time_api_call = timestamp()
-    status_price, data_price = make_request(url_base, url_query2)
+        # organize dataframe
+        if boolean_column_name != None:
+            
+            df = select_in_boolean_column(df, boolean_column_name, boolean_column_bool)
 
-    # check connection api
-    if status_price != 200:
-
-        print("Error API")
-    
-    else:
-
-        # organize data into a dataframe
-        df_price = dictionnary_to_dataframe(data_price)
-        df_price = dictonnary_to_multi_columns(df_price, "rates", True)
+        if column_dict_name != None:
+            
+            df = dictonnary_to_multi_columns(df, column_dict_name, column_dict_replace)
 
         # add timestamp
-        df_price["timestamp"] = time_api_call
+        if timestamp_bool == True:
+            
+            df["timestamp"] = time_api_call
 
         # save dataset
-        df_price.to_parquet("data/df_price.gzip", compression='gzip')
+        df.to_parquet(filename + ".gzip", compression="gzip")
+    
+        return df, status
+    
+
+#### Merge two dataframe and save the result ####
+def creation_df_final(params_url1, params_url2, filename_df_final):
+
+    df1, status_1 = data_api_to_dataframe(params_url1["url_base"], params_url1["url_query"], params_url1["filename"], params_url1["list_index_json"], params_url1["timestamp_bool"], params_url1["boolean_column_name"], params_url1["boolean_column_bool"], params_url1["column_dict_name"], params_url1["column_dict_replace"])
+    
+    df2, status_2 = data_api_to_dataframe(params_url2["url_base"], params_url2["url_query"], params_url2["filename"], params_url2["list_index_json"], params_url2["timestamp_bool"], params_url2["boolean_column_name"], params_url2["boolean_column_bool"], params_url2["column_dict_name"], params_url2["column_dict_replace"])
     
     # check connection api
-    if status_price != 200 or status_currencies_code != 200:
+    if status_2 != 200 or status_1 != 200:
 
         print("Error API")
 
     else:
 
         # merge df_price and df_currency_code (inner join)
-        df_final = df_currency_code.merge(df_price, left_index = True, right_index = True)
+        df_final = df1.merge(df2, left_index = True, right_index = True)
 
         # data quality (dtypes, number of nan in each column, duplicate rows removed)
         """
@@ -165,4 +175,4 @@ def creation_df_final(url_base, url_query1, url_query2):
         df_final.set_index("timestamp", inplace = True)
 
         # save dataset
-        df_final.to_parquet("data/df_final.gzip", compression='gzip')
+        df_final.to_parquet(filename_df_final + ".gzip", compression="gzip")
